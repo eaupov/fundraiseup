8dfd62f1a65c668eda858b7a513d4fbf22e1d2a7
Emil <eaupov@gmail.com>
2021-10-26 00:59:14 +0500

    Основная часть готова. Осталось доделать некоторые пункты из раздела "Детали".

---
+++ src/js/script.js
@@ -0,0 +1,23 @@
+$( function() {
+	$("#border-size").slider({
+		range: "max",
+		min: 1,
+		max: 10,
+		value: 2,
+		slide: function(event, ui) {
+			$("#border-size-value").text(ui.value+'px');
+		}
+	});
+	$("#border-size-value").text($("#border-size").slider("value")+'px');
+
+	$("#border-radius").slider({
+		range: "max",
+		min: 1,
+		max: 100,
+		value: 15,
+		slide: function(event, ui) {
+			$("#border-radius-value").text(ui.value+'px');
+		}
+	});
+	$("#border-radius-value").text($("#border-radius").slider("value")+'px');
+});
